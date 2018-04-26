7b4a0911944d591785b87677a4b496e521cb7220
Juan Roca <juanroca@appmeup.co>
2018-04-26 13:00:54 -0500

    dbUrl updated

--- src/firebase/ApiKeys.js
+++ src/firebase/ApiKeys.js
@@ -2,7 +2,7 @@ export default{
   FirebaseConfig:{
     apiKey: "AIzaSyAxrvmpgLh2eYfprjKNzfnTnVcEzV4mwAs",
     authDomain: "firulais-app.firebaseapp.com",
-    databaseURL: "https:firulais-app.firebaseio.com",
+    databaseURL: "https://firulais-app.firebaseio.com",
     projectId: "firulais-app",
     storageBucket: "firulais-app.appspot.com",
     messagingSenderId: "689840315607"
