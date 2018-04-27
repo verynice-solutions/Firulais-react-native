083dc656b0b00d2742bbbde1b3e08cd00612a880
Juan Roca <juanroca@appmeup.co>
2018-04-26 19:57:42 -0500

    fixed android google crash

--- src/config/OAuth.js
+++ src/config/OAuth.js
@@ -3,7 +3,7 @@ const OAuthIds = {
   options: {
     behavior:'web',
     iosClientId:'689840315607-psj3sjeso0suee4k2hntdk65d5nfrub4.apps.googleusercontent.com',
-    android:'689840315607-rj4h96rla4fmbc0rgk0bk2p9n0c2rrvr.apps.googleusercontent.com',
+    androidClientId:'689840315607-rj4h96rla4fmbc0rgk0bk2p9n0c2rrvr.apps.googleusercontent.com',
     scopes:['https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email'] // CONTACTS? 'https://www.googleapis.com/auth/contacts.readonly'
   }
